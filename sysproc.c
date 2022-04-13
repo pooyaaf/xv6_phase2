adfcaff3ee61043a179eae79281d2d038ddc4238
mdyErfani <mdyerfani@ut.ac.ir>
2022-04-13 17:10:55 +0430

    creepy bitch

--- sysproc.c
+++ sysproc.c
@@ -89,39 +89,10 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
-// ---  prime
-int isPrime(int n)
-{
-    // Corner cases
-    if (n <= 1)  return 0;
-    if (n <= 3)  return 1;
-   
-    // This is checked so that we can skip 
-    // middle five numbers in below loop
-    if (n%2 == 0 || n%3 == 0) return 0;
-   
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return 0;
-   
-    return 1;
-}
+
 int
 sys_find_prime_number(void)
 {
-  int num;
-  num = myproc()->tf->ebx;
-  int prime = num ; 
-  int found = 0;
-     
-  while(!found)
-    {
-        prime++;
-        if(isPrime(prime))
-        {
-            found = 1;
-        }
-    }
-  return prime;
-
+  int num = myproc()->tf->ebx;
+  return find_prime_number(num);
 }
--- test_find_prime_number.c
+++ test_find_prime_number.c
@@ -4,15 +4,24 @@
 
 int main(int argc,char *argv[])
 {
+	if(argc != 2)
+    {
+	    printf(1, "Please enter only one argument\n");
+		exit();
+	}
+
     int num = atoi(argv[1]);
-    asm("mov %0, %%ebx;"
-    :
-    :"r"(num)
+    int prev_value = 0;
+
+    asm volatile(
+        "movl %%ebx, %0;"
+        "movl %1, %%ebx;"
+        : "=r" (prev_value)
+        : "r"(num)
     );
-    
-    //
-    
-    printf(1,"Next prime is -->  %d\n",find_prime_number());
-    
+
+    printf(1, "next prime number = %d\n" , find_prime_number());
+
+    asm("movl %0, %%ebx" : : "r"(prev_value));
     exit();
 }
