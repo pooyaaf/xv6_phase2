e16920c7c09e409c2b2636519a933ebcf78ba474
pooyaaf <pooya1380@gmail.com>
2022-04-17 15:16:07 +0430

    call_count_finished

--- test_get_call_count.c
+++ test_get_call_count.c
@@ -2,29 +2,51 @@
 #include "stat.h"
 #include "user.h"
 #include "syscall.h"
+#include "fcntl.h"
 
-void print_syscalls(char* pname) {
-    printf(1, "%s - fork count: %d ", pname, get_call_count(SYS_fork));
-    printf(1, "%s - write count: %d\n", pname, get_call_count(SYS_write));
-}
+
+// void print_syscalls(int fd,char* pname) {
+//     printf(fd, "%s - fork count: %d ", pname, get_call_count(SYS_fork));
+//     printf(fd, "%s - write count: %d\n", pname, get_call_count(SYS_write));
+// }
 
 int main(int argc,char *argv[])
 {
-    if (fork() == 0) {
-        print_syscalls("first child");
+    int parent,child1,child2;
+    int fork_sol,write_sol;
+    if(argc<1)
         exit();
+    parent = getpid();
+    child1 = fork();
+    if(child1 == 0)
+    {
+        write(1,"Child1 writes s.t. in command!\n",31);
+        fork_sol = get_call_count(SYS_fork);
+        write_sol = get_call_count(SYS_write);
+        printf(1,"Child1 Process Fork status:%d,Write status:%d \n",fork_sol,write_sol);
+        kill(child1);
     }
-    else {
-        if (fork() == 0) {
-            print_syscalls("second child");
-            exit();
-        }
-        else {
-            wait();
-            print_syscalls("parent");
-            exit();
-        }
-    }
+    sleep(10);
+    child2 = fork();
 
+    fork_sol = get_call_count(SYS_fork);
+    write_sol = get_call_count(SYS_write);
+            
+    wait();
+    if(child2 == 0){
+        printf(1,"Child2 Process Fork status:%d,Write status:%d \n",fork_sol,write_sol);
+    }
+    else if( (child1!=0) && (child2!=0))
+    {
+        wait();
+        wait();
+        //
+        printf(1,"Main Process ID: %d \n",parent);
+        printf(1,"CHILD ID's 1:%d,2:%d \n",child1,child2);
+        printf(1,"Child1 Fork status:%d,Write status:%d \n",fork_sol,write_sol);
+        //
+        for(int i=0;i<200;i=i+1);
+    }
     exit();
+    return 0;
 }
